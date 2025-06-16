9c937aee39efffdc367ae3e304088d1e06cb87b0
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-06-16 15:31:26 +0000

    PixelPropsUtil: Cleanup! PlayIntegrity fixer
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -482,20 +482,6 @@ public class PixelPropsUtils {
         return gmsUid == callingUid;
     }
 
-    private static boolean isCallerSafetyNet() {
-        return Arrays.stream(Thread.currentThread().getStackTrace())
-                        .anyMatch(elem -> elem.getClassName().toLowerCase()
-                            .contains("droidguard"));
-    }
-
-    public static void onEngineGetCertificateChain() {
-        // Check stack for SafetyNet or Play Integrity
-        if (isCallerSafetyNet() || sIsFinsky) {
-            Log.i(TAG, "Blocked key attestation");
-            throw new UnsupportedOperationException();
-        }
-    }
-
     public static void dlog(String msg) {
         if (DEBUG) Log.d(TAG, msg);
     }
